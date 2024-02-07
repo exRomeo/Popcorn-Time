433a55706f232636e6a2ad942dcbe8c5f027b818
Ramy Ashraf <ramy3r@hotmail.com>
2024-02-07 20:10:01 +0200

    Updating dependencies (#6)
    * updated gradle dependencies and removed deprecated accompanist libraries and refactored the code accordingly
    
    * removed kapt and added ksp

--- app/src/main/java/com/example/popcorntime/MainActivity.kt
+++ app/src/main/java/com/example/popcorntime/MainActivity.kt
@@ -3,20 +3,18 @@ package com.example.popcorntime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.navigation.compose.rememberNavController
 import com.example.popcorntime.core.navigation.MainNavigation
 import com.example.popcorntime.ui.theme.PopcornTimeTheme
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PopcornTimeTheme {
-                MainNavigation(navController = rememberAnimatedNavController())
+                MainNavigation(navController = rememberNavController())
             }
         }
     }
